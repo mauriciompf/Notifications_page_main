87ba69550dca721f5afff042192bcc9187cd7e9b
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-06-29 21:01:12 -0400

    Update app.js

---
+++ src/assets/js/app.js
@@ -0,0 +1,19 @@
+const items = document.querySelectorAll("[data-item='item']");
+const countNumbers = document.querySelectorAll("[data-number='count']");
+const textMessages = document.querySelectorAll(".message__text");
+const ballsRed = document.createElement("span");
+ballsRed.appendChild(ballsRed);
+
+// textMessages.forEach((text) => {
+//   text.appendChild(ballsRed);
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  //   textMessages.forEach((text) => {
+  //     ballsRed.appendChild(text);
+  //     // console.log(text);
+  //   });
+  //   ballsRed.forEach((ball) => {
+  // ball.classList.toggle("show");
+  //   });
+});
