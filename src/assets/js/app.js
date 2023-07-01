76255aea0eb140bbc5526bab7820686873949012
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-01 19:58:19 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,19 +1,32 @@
-const items = document.querySelectorAll("[data-item='item']");
-const countNumbers = document.querySelectorAll("[data-number='count']");
-const textMessages = document.querySelectorAll(".message__text");
-const ballsRed = document.createElement("span");
-ballsRed.appendChild(ballsRed);
-
-// textMessages.forEach((text) => {
-//   text.appendChild(ballsRed);
-// });
+const markAllItems = document.querySelector("[data-button='button']");
+const items = Array.from(document.querySelectorAll("[data-item='item']"));
+const textMessages = Array.from(document.querySelectorAll(".message__text"));
+const countNumbers = document.querySelector("[data-number='count']");
 
 window.addEventListener("DOMContentLoaded", () => {
-  //   textMessages.forEach((text) => {
-  //     ballsRed.appendChild(text);
-  //     // console.log(text);
-  //   });
-  //   ballsRed.forEach((ball) => {
-  // ball.classList.toggle("show");
-  //   });
+  countNumbers.textContent = items.length;
+
+  textMessages.forEach((text) => {
+    const ballsRed = document.createElement("span");
+    ballsRed.className = "span";
+    text.appendChild(ballsRed);
+  });
+
+  items.forEach((item) => {
+    item.classList.add("active");
+  });
+});
+
+markAllItems.addEventListener("click", () => {
+  const balls = [];
+  items.forEach((item) => {
+    item.classList.remove("active");
+    balls.push(...item.querySelectorAll(".span"));
+  });
+
+  balls.forEach((ball) => {
+    ball.style.display = "none";
+  });
+
+  countNumbers.textContent = 0;
 });
